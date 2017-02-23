142de08b7725f926f463c50ed0197190fc795f28
CarroCarrillo <dcm3651@gmail.com>
2017-02-23 18:59:06 +0100

    Ejemplo de Image Crawler

--- src/main.java
+++ src/main.java
@@ -1,10 +1,17 @@
 import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.examples.imagecrawler.ImageCrawler;
+import edu.uci.ics.crawler4j.url.WebURL;
 
 public class main {
 
 	public static void main(String[] args) {
-		Crawler c = new Crawler();
-		Page p = new Page();
+		ImageCrawler ic = new ImageCrawler();
+		WebURL url = new WebURL();
+		url.setURL("http://google.com");
+		Page referringPage = new Page(url);
+		ic.shouldVisit(referringPage, url);
+		ic.visit(referringPage);
+		ic.run();
 
 	}
 
